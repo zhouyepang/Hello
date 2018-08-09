4349c334b2e898763065791e80a0c69239325c3d
zhouyepang <pangtian2006@gmail.com>
2018-08-09 12:51:59 +1000

    service

---
+++ app/src/main/java/com/example/zhouyepang/hello/MyService.java
@@ -0,0 +1,23 @@
+package com.example.zhouyepang.hello;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("ServiceWords", "Alive");
+    }
+
+}
